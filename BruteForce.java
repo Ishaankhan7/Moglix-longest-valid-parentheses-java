11ae8d48851cb86fe420c6086ba9b12ab21e1e26
Ishaan Khan <122585129+Ishaankhan7@users.noreply.github.com>
2026-07-21 12:23:08 +0530

    Implement brute force solution for valid parentheses

---
+++ BruteForce.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestValidParenthesesBruteForce(String s) {
+        int maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 2; j <= s.length(); j += 2) {
+                if (isValid(s.substring(i, j))) {
+                    maxLen = Math.max(maxLen, j - i);
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    private boolean isValid(String s) {
+        int balance = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') balance++;
+            else balance--;
+            if (balance < 0) return false;
+        }
+        return balance == 0;
+    }
+}
